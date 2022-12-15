a023749a89e355e1b1caa9cc12fe4f9d97e4b431
Lucifer <tim.noelmans@outlook.com>
2022-12-15 04:51:51 +0100

    Branding, Logos and so on

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
 
+//screens
+import DetailScreen from './screens/Details';
 import HomeScreen from './screens/Home'; 
 
 const Stack = createNativeStackNavigator();
@@ -8,18 +10,18 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="home" component={HomeScreen} />
+      <Stack.Navigator screenOptions={{
+        headerStyle: {
+          backgroundColor: "F4F4F4",
+        }
+      }}>
+        <Stack.Screen 
+          name="Home" 
+          component={HomeScreen}
+          options={{title: "BiteFind"}}
+        />
+        <Stack.Screen name="Details" component={DetailScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- screens/Home.js
+++ screens/Home.js
@@ -1,9 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 const HomeScreen = ({ navigation }) => {
     return(
-        <View>
+        <View style={{backgroundColor: "F4F4F4", flex: 1}}>
             <Text>Hello World!!</Text>
+            <StatusBar/>
         </View>
     )
 };
