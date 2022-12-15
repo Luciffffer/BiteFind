dd8e05e6e655390749302a032036dca932e54606
Lucifer <tim.noelmans@outlook.com>
2022-12-15 04:21:09 +0100

    added homescreen and navigation

---
+++ screens/Home.js
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+const HomeScreen = ({ navigation }) => {
+    return(
+        <View>
+            <Text>Hello World!!</Text>
+        </View>
+    )
+};
+
+export default HomeScreen;
