a32a990e9363901fdc85122e882bb41408209635
Lucifer <tim.noelmans@outlook.com>
2022-12-15 04:22:47 +0100

    added details screen file

---
+++ screens/Details.js
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+const DetailScreen = ({ navigation }) => {
+    return(
+        <View>
+            <Text>These are the details</Text>
+        </View>
+    )
+};
+
+export default DetailScreen;
